cca9ded53f0ccc754d1b8b725432a0c5f8c926b7
Nate I. <nate.isern.alvarez@gmail.com>
2026-09-10 02:26:56 +0300

    chore: roll out anonymous activity ids (#17)

--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-Oxy-Edge-Region"
+    "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-Oxy-Edge-Region, X-Oxy-Activity-Id"
   );
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
